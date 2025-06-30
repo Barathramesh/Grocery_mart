8a5ce6f012e2f6a9ec0ba80282cc75f6ec5540f6
Barathramesh <barathramesh26@gmail.com>
2025-06-30 19:54:23 +0530

    Login Api Call

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -37,3 +37,41 @@ export const register = async (req, res)=>{
         return res.status(500).json({success: false, message: error.message});
     }
 }
+
+//Login User : /api/user/login
+export const login = async (req, res)=>{
+    try {
+        const {email, password} = req.body;
+
+        if(!email || !password) {
+            return res.status(400).json({success:false, message:'Email and password are required !'});
+        }
+        
+        const user = await User.findOne({email});
+
+        if(!user) {
+            return res.status(400).json({success:false, message:'Invalid email or password'});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch) {
+           return res.status(400).json({success:false, message:'Password'});
+        }
+        
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
+
+        res.cookie('token',token, {
+            httpOnly: true, 
+            secure: process.env.NODE_ENV === 'production', 
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', 
+            maxAge: 7 * 24 * 60 * 60 * 1000, 
+        })
+  
+         return res.status(201).json({success: true, user: {_id: user._id, name: user.name, email: user.email, createdAt: user.createdAt}})
+
+    } catch (error) {
+         console.error("Login error: ",error.message);
+        return res.status(500).json({success: false, message: error.message});
+    }
+}
