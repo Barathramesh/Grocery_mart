1f00e5677a5311f2980a5d87de206796843da866
Barathramesh <barathramesh26@gmail.com>
2025-07-09 09:50:49 +0530

    Address Connectivity

--- server/controllers/addressController.js
+++ server/controllers/addressController.js
@@ -1,26 +1,34 @@
 import Address from "../models/Address.js";
 
-
 // Add Address : /api/address/add
 export const addAddress = async (req, res) => {
     try {
         const {userId, address} = req.body;
-        await Address.create(...address,userId)
-        return res.json({success: true, message : "Address added successfully"});
+        if (!req.body || !req.body.userId || !req.body.address) {
+            return res.status(400).json({
+                success: false,
+                message: "Missing required fields: userId and address"
+            });
+        }
+    
+        const newAddress = await Address.create(
+            {userId,
+            ...address})
+        res.json({success: true, message : "Address added successfully",address: newAddress});
     } catch (error) {
-        console.log("Add Address failed:", error.message);
-        return res.json({success: false, message: error.message});
+         console.log("Add Address failed:", error.message);
+         res.json({success: false, message: error.message});
     }
 }
 
 // Get Address : /api/address/get
 export const getAddress = async (req, res) => {
     try {
-        const {userId} = req.body;
+        const {userId} = req.query;
         const addresses = await Address.find({userId});
-        return res.json({success: true, addresses });
+        res.json({success: true, addresses });
     } catch (error) {
         console.log("Get Address failed:", error.message);
-        return res.json({success: false, message: error.message});
+        res.json({success: false, message: error.message});
     }
 }
